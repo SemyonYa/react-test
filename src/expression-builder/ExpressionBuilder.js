fb9926eca28e00eda17c18cd0d6ae6b64ffe5358
unknown <mygarbage86@mail.ru>
2021-06-28 00:29:00 +0300

    added d&d

--- src/expression-builder/ExpressionBuilder.js
+++ src/expression-builder/ExpressionBuilder.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import './ExpressionBuilder.css';
+import { Draggable, Droppable } from 'react-drag-and-drop';
 
 function ExpressionBuilder(props) {
-    return <h1>qweqweqwe</h1>;
+    function onDrop(data) {
+        console.log(data)
+        // => banana 
+    }
+
+    return <div className='wrap'>
+        <div className='header'>
+            <Draggable type="fruit" data="banana"><li>Banana</li></Draggable>
+            <Draggable type="fruit" data="apple"><li>Apple</li></Draggable>
+            <Draggable type="metal" data="silver"><li>Silver</li></Draggable>
+        </div>
+        <div className='expression'>
+            <Droppable
+                types={['fruit']} // <= allowed drop types
+                onDrop={onDrop}
+            >
+                <ul className="Smoothie"></ul>
+            </Droppable>
+        </div>
+    </div>;
+
 }
 
 export default ExpressionBuilder
