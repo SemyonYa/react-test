5afc19e592e35feb5c9c1d64f7565159f0a9b2c2
unknown <mygarbage86@mail.ru>
2021-06-28 00:06:50 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpressionBuilder from './expression-builder/ExpressionBuilder'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>First REACT</h1>
+      <ExpressionBuilder />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/expression-builder/ExpressionBuilder.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ExpressionBuilder(props) {
+    return <h1>qweqweqwe</h1>;
+}
+
+export default ExpressionBuilder
