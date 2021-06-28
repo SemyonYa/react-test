6b83f09ef1f06d6619a0c9b1b3394119cf8518c1
SemyonYa <mygarbage86@mail.ru>
2021-06-28 17:57:40 +0300

    d&d with node creation

--- src/expression-builder/ExpressionBuilder.js
+++ src/expression-builder/ExpressionBuilder.js
@@ -2,28 +2,72 @@ import React from 'react';
 import './ExpressionBuilder.css';
 import { Draggable, Droppable } from 'react-drag-and-drop';
 
-function ExpressionBuilder(props) {
-    function onDrop(data) {
-        console.log(data)
-        // => banana 
+const types = {
+    plus: 'plus',
+    brackets: 'brackets',
+    num: 'num',
+};
+
+class ExpressionBuilder extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    onDrop(data) {
+        console.log(typeof data, data);
+        if (data['operator'] === types.plus) {
+            console.log(this);
+            const node = this.generateNode(types.plus);
+            this.insertNodeToExpression(node);
+        } else if (data['operator'] === types.brackets) {
+            console.log('()');
+            const node = this.generateNode(types.brackets);
+            this.insertNodeToExpression(node);
+        } else if (data['value'] === types.num) {
+            console.log('num');
+            const node = this.generateNode(types.num);
+            this.insertNodeToExpression(node);
+        }
     }
 
-    return <div className='wrap'>
-        <div className='header'>
-            <Draggable type="fruit" data="banana"><li>Banana</li></Draggable>
-            <Draggable type="fruit" data="apple"><li>Apple</li></Draggable>
-            <Draggable type="metal" data="silver"><li>Silver</li></Draggable>
-        </div>
-        <div className='expression'>
-            <Droppable
-                types={['fruit']} // <= allowed drop types
-                onDrop={onDrop}
-            >
-                <ul className="Smoothie"></ul>
-            </Droppable>
-        </div>
-    </div>;
+    render() {
+        return (
+            <div className='wrap'>
+                <div className='header'>
+                    <span className='header-caption'>Элементы конструктора выражения:</span>
+                    <span className='header-list'>
+                        <Draggable type="operator" data="plus">+</Draggable>
+                        <Draggable type="operator" data="brackets">()</Draggable>
+                        <Draggable type="value" data="num">num</Draggable>
+                    </span>
+                </div>
+                <Droppable types={['value', 'operator']} onDrop={this.onDrop.bind(this)}><div className='expression'></div></Droppable>
+            </div>
+            );
+    }
 
+    generateNode(elemType) {
+        let draggableNode = document.createElement('div');
+        draggableNode.setAttribute('type', 'value');
+        draggableNode.setAttribute('draggable', 'true');
+        draggableNode.classList.add('expression-node');
+        if (elemType === types.plus) {
+            draggableNode.setAttribute('data', types.plus);
+            draggableNode.innerText = '+'
+        } else if (elemType === types.brackets) {
+            draggableNode.setAttribute('data', types.brackets);
+            draggableNode.innerText = '()';
+        } else if (elemType === types.num) {
+            draggableNode.setAttribute('data', types.num);
+            draggableNode.innerText = 'num';
+        }
+        return draggableNode;
+    }
+
+    insertNodeToExpression(node) {
+        let expressionNode = document.querySelector('.expression');
+        expressionNode.append(node);
+    }
 }
 
 export default ExpressionBuilder
