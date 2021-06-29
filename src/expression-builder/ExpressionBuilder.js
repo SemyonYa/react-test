7f9b886f15def36afaf3764c94be8ce86f71105f
SemyonYa <mygarbage86@mail.ru>
2021-06-29 17:50:21 +0300

    ---

--- src/expression-builder/ExpressionBuilder.js
+++ src/expression-builder/ExpressionBuilder.js
@@ -1,33 +1,49 @@
 import React from 'react';
 import './ExpressionBuilder.css';
-import { Draggable, Droppable } from 'react-drag-and-drop';
+// import { Draggable, Droppable } from 'react-drag-and-drop';
 
 const types = {
-    plus: 'plus',
+    operator: 'plus',
     brackets: 'brackets',
     num: 'num',
+    model: 'model'
 };
+
+class Fake {
+    id = null;
+    name = null;
+    age = null;
+}
 
 class ExpressionBuilder extends React.Component {
     constructor(props) {
         super(props);
     }
-    
-    onDrop(data) {
-        console.log(typeof data, data);
-        if (data['operator'] === types.plus) {
-            console.log(this);
-            const node = this.generateNode(types.plus);
-            this.insertNodeToExpression(node);
-        } else if (data['operator'] === types.brackets) {
-            console.log('()');
-            const node = this.generateNode(types.brackets);
-            this.insertNodeToExpression(node);
-        } else if (data['value'] === types.num) {
-            console.log('num');
-            const node = this.generateNode(types.num);
-            this.insertNodeToExpression(node);
-        }
+
+    activeType = '';
+    currentTarget = null;
+    expressionData = [];
+
+    componentDidMount() {
+        this.buildExpression();
+        let drags = document.querySelectorAll('.parts-item');
+        drags.forEach((elem, index) => {
+            elem.draggable = true;
+            elem.addEventListener('dragstart', (e) => {
+                this.activeType = e.target.getAttribute('data-type');
+            });
+            elem.addEventListener('dragend', (e) => {
+                const indexForNode = this.currentTarget?.getAttribute('data-index');
+                if (this.activeType && this.currentTarget) {
+                    this.pushNodesToExpressionData(this.generateNodes(this.activeType), indexForNode);
+                    this.buildExpression();
+                }
+                this.activeType = '';
+            });
+            elem.addEventListener('drag', (e) => {
+                this.currentTarget = null;
+            });
+        });
     }
 
     render() {
@@ -35,38 +51,146 @@ class ExpressionBuilder extends React.Component {
             <div className='wrap'>
                 <div className='header'>
                     <span className='header-caption'>Элементы конструктора выражения:</span>
-                    <span className='header-list'>
-                        <Draggable type="operator" data="plus">+</Draggable>
-                        <Draggable type="operator" data="brackets">()</Draggable>
-                        <Draggable type="value" data="num">num</Draggable>
-                    </span>
+                    <div className='parts'>
+                        <div className='parts-item' data-type="plus">+</div>
+                        <div draggable='true' className='parts-item' data-type="brackets">()</div>
+                        <div draggable='true' className='parts-item' data-type="num">num</div>
+                        <div draggable='true' className='parts-item' data-type="model">model.prop</div>
+                    </div>
                 </div>
-                <Droppable types={['value', 'operator']} onDrop={this.onDrop.bind(this)}><div className='expression'></div></Droppable>
+                <div className='expression'></div>
+                <div className='expression2'></div>
             </div>
             );
     }
 
-    generateNode(elemType) {
-        let draggableNode = document.createElement('div');
-        draggableNode.setAttribute('type', 'value');
-        draggableNode.setAttribute('draggable', 'true');
-        draggableNode.classList.add('expression-node');
-        if (elemType === types.plus) {
-            draggableNode.setAttribute('data', types.plus);
-            draggableNode.innerText = '+'
-        } else if (elemType === types.brackets) {
-            draggableNode.setAttribute('data', types.brackets);
-            draggableNode.innerText = '()';
-        } else if (elemType === types.num) {
-            draggableNode.setAttribute('data', types.num);
-            draggableNode.innerText = 'num';
+    generateNodes(elemType) {
+        let nodes = [];
+        if (elemType === types.brackets) {
+            nodes.push(getDraggableNode('(', null, 'left'));
+            nodes.push(getDraggableNode(')', null, 'right'));
+        } else {
+            if (elemType === types.operator) {
+                let ddList = document.createElement('select');
+                ddList.addEventListener('change', (e) => {
+                    e.target.setAttribute('data-description', e.target.value);
+                    this.expressionToString();
+                });
+                ddList.append(...['+', '-', '>', '<', '>=', '<=', '=='].map((item) => {
+                    let option = document.createElement('option');
+                    option.innerText = item;
+                    option.value = item;
+                    return option;
+                }));
+                nodes.push(getDraggableNode(null, ddList, '+'));
+            } else if (elemType === types.num) {
+                let input = document.createElement('input');
+                input.value = 1;
+                input.addEventListener('input', (e) => {
+                    this.expressionToString();
+                });
+                nodes.push(getDraggableNode(null, input));
+            } else if (elemType === types.model) {
+                let ddList = document.createElement('select');
+                const props = [];
+                // TODO: 
+                const fake = new Fake();
+                for (let prop in fake) {
+                    console.log(prop);
+                    props.push(prop);
+                }
+                ddList.append(...props.map((p) => {
+                    let option = document.createElement('option');
+                    option.innerText = `${Fake.name}.${p}`;
+                    option.value = p;
+                    return option;
+                }));
+
+                ddList.addEventListener('change', (e) => {
+                    e.target.setAttribute('data-description', e.target.value);
+                    this.expressionToString();
+                });
+
+                nodes.push(getDraggableNode(null, ddList, props[0]));
+            }
         }
-        return draggableNode;
+
+        function getDraggableNode(innerText, draggableNode, description) {
+            if (!draggableNode) {
+                draggableNode = document.createElement('div');
+                draggableNode.innerText = innerText;
+            }
+            draggableNode.setAttribute('draggable', 'true');
+            draggableNode.classList.add('expression-node');
+            draggableNode.setAttribute('data-type', elemType);
+            if (description)
+                draggableNode.setAttribute('data-description', description);
+            return draggableNode;
+        }
+        return nodes;
     }
 
-    insertNodeToExpression(node) {
+    generateDroppable(dataNodeIndex = null) {
+        let droppableNode = document.createElement('div');
+        droppableNode.classList.add('expression-droppable');
+        droppableNode.draggable = true;
+        if (dataNodeIndex)
+            droppableNode.setAttribute('data-index', dataNodeIndex);
+        droppableNode.addEventListener('dragover', (e) => {
+                e.preventDefault();
+                this.currentTarget = e.target;
+            });
+        return droppableNode;
+    }
+
+    appendNodeToExpression(node) {
         let expressionNode = document.querySelector('.expression');
         expressionNode.append(node);
+    }
+
+    clearExpression() {
+        document.querySelector('.expression').innerHTML = null;
+    }
+
+    pushNodesToExpressionData(nodes, index = null) {
+         if (index !== null) {
+            this.expressionData = [
+                ...this.expressionData.slice(0, index),
+                ...nodes,
+                ...this.expressionData.slice(index),
+            ];
+         } else {
+             this.expressionData.push(...nodes);
+         }
+    }
+
+    buildExpression() {
+        this.clearExpression();
+        this.appendNodeToExpression(this.generateDroppable('0'));
+        this.expressionData.forEach((elem, index) => {
+            this.appendNodeToExpression(elem);
+            this.appendNodeToExpression(this.generateDroppable(index + 1));
+        });
+
+        this.expressionToString();
+    }
+
+    expressionToString() {
+        const expressionString = this.expressionData.map((elem, index) => {
+            const currentType = elem.getAttribute('data-type');
+            if (currentType === types.operator || currentType === types.model) {
+                return elem.getAttribute('data-description');
+            } else if (currentType === types.num) {
+                return elem.value;
+            } else if (currentType === types.brackets) {
+                if (elem.getAttribute('data-description') === 'left') {
+                    return '(';
+                } else {
+                    return ')';
+                }
+            }
+        }).join(' ');
+        document.querySelector('.expression2').innerText = expressionString;
     }
 }
 
